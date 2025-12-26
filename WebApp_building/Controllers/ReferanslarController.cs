8ea12742101a460db003cf661993697cacf7260a
resulkarademir <resulkarademir23@gmail.com>
2025-12-26 14:31:13 +0300

    Comlated All Controller

--- WebApp_building/Controllers/ReferanslarController.cs
+++ WebApp_building/Controllers/ReferanslarController.cs
@@ -1,12 +1,49 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApp_building.DAL.Context;
+using WebApp_building.DAL.Entities;
 
 namespace WebApp_building.Controllers
 {
     public class ReferanslarController : Controller
     {
+        DataBaseContext _context = new DataBaseContext();
         public IActionResult Index()
         {
+            var values = _context.Referanslars.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult ReferansEkle()
+        {
             return View();
+        }
+        [HttpPost]
+        public IActionResult ReferansEkle(Referanslar referanslar)
+        {
+            _context.Referanslars.Add(referanslar);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult ReferansGuncelle(int id)
+        {
+            var referanslar = _context.Referanslars.Find(id);
+            return View(referanslar);
+        }
+        [HttpPost]
+        public IActionResult ReferansGuncelle(Referanslar referanslar)
+        {
+            _context.Referanslars.Update(referanslar);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult ReferansSil(int id)
+        {
+            var referanslar = _context.Referanslars.Find(id);
+            _context.Referanslars.Remove(referanslar);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
--- WebApp_building/Controllers/SorularController.cs
+++ WebApp_building/Controllers/SorularController.cs
@@ -1,12 +1,49 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApp_building.DAL.Context;
+using WebApp_building.DAL.Entities;
 
 namespace WebApp_building.Controllers
 {
     public class SorularController : Controller
     {
+        DataBaseContext _context = new DataBaseContext();
         public IActionResult Index()
         {
+            var values = _context.Asks.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult SoruEkle()
+        {
             return View();
+        }
+        [HttpPost]
+        public IActionResult SoruEkle(Ask ask)
+        {
+            _context.Asks.Add(ask);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult SoruGuncelle(int id)
+        {
+            var ask = _context.Asks.Find(id);
+            return View(ask);
+        }
+        [HttpPost]
+        public IActionResult SoruGuncelle(Ask ask)
+        {
+            _context.Asks.Update(ask);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult SoruSil(int id)
+        {
+            var ask = _context.Asks.Find(id);
+            _context.Asks.Remove(ask);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
